3135ab4556cebe431911924eaf80eb2680b559c0
amd5181 <adavidfr2006@gmail.com>
2025-06-04 12:41:30 -0400

    Update sheetAuth.js

--- frontend/api/sheetAuth.js
+++ frontend/api/sheetAuth.js
@@ -1,1 +1,13 @@
-hold
+const { google } = require('googleapis');
+
+async function getAuthClient() {
+  const credentials = JSON.parse(process.env.GOOGLE_CREDENTIALS_JSON);
+  const auth = new google.auth.GoogleAuth({
+    credentials,
+    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+  });
+  return auth;
+}
+
+module.exports = { getAuthClient };
+
